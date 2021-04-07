7c0f7f7c9b8c04ed13e7422485b7154ea49f2560
leedoyeon <gcdalee@gmail.com>
2021-04-07 19:23:18 +0900

    memo layout

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.ExifInterface
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
